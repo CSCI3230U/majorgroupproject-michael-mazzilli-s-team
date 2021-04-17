f83f4a3e6308676d03a0c486bb5837589ceba805
Justin Katerberg <j.katerberg10@gmail.com>
2021-04-17 18:09:37 -0400

    validation for adding users

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -1,23 +1,39 @@
 //imports
-const mongoose=require('mongoose');
+var mongoose=require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 //generate database schema
 let Schema = mongoose.Schema;
 let userSchema = new Schema({
-    uid: String,
-    username: String,
+    uid: {
+        type:String,
+        require: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        require: true
+    },
     name: {
-        first: String,
-        last: String
+        type: Object,
+        require: true,
+        type: {
+            first: String,
+            last: String
+        }
     },
     friends: [{
         uid: String, 
         username: String
     }],
-    picture: String,
+    picture: {
+        type: String,
+        default: `https://robohash.org/default`
+    },
     last_login: Date,
     settings: Array
 })
 
 //export model for use later
+userSchema.plugin(uniqueValidator, { message: "Username taken" });
 module.exports.Users = mongoose.model('users', userSchema);
--- server/routes/adduser.js
+++ server/routes/adduser.js
@@ -4,6 +4,7 @@ var userModel = require('../models/user_model');
 var loginModel = require('../models/login_model');
 var {v4 : uuidv4} = require('uuid');
 var bcrypt = require('bcrypt');
+var { check, validationResult } = require('express-validator');
 
 /**
  * Adds new user to the database. Creates a document in the user collection and login collection
@@ -17,47 +18,67 @@ var bcrypt = require('bcrypt');
  * Command to test:
  *  curl -X POST -H "Content-Type: application/json" -d '{"username":"test", "first":"Josh", "last":"Slosh", "password":"password"}' http://localhost:3000/adduser
  */
-router.post('/adduser', (req,res) => {
-    var newUID = uuidv4(); //generate unique user ID
+router.post('/adduser', 
+    [
+        check('username', 'username is required')
+            .not()
+            .isEmpty(),
+        check('password', 'password is required')
+            .not()
+            .isEmpty(),
+        check('first', 'first is required: supply user first name')
+            .not()
+            .isEmpty(),
+        check('last', 'last is required: supply user last name')
+            .not()
+            .isEmpty()
+    ], 
+    (req,res) => {
+        var errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(422).jsonp(errors.array());
+        }else{
+            var newUID = uuidv4(); //generate unique user ID
 
-    //hash the password right away
-    bcrypt.hash(req.body.password, 10).then((hash) => {
-        //Create a new user document using the existing schema
-        const newUser = new userModel.Users({
-            uid: newUID,
-            username: req.body.username,
-            name: {
-                first: req.body.first,
-                last: req.body.last
-            },
-            picture: `https://robohash.org/${newUID}`,
-            last_login: Date.now()
-        })
+            //hash the password right away
+            bcrypt.hash(req.body.password, 10).then((hash) => {
+                //Create a new user document using the existing schema
+                const newUser = new userModel.Users({
+                    uid: newUID,
+                    username: req.body.username,
+                    name: {
+                        first: req.body.first,
+                        last: req.body.last
+                    },
+                    picture: `https://robohash.org/${newUID}`,
+                    last_login: Date.now()
+                })
 
-        //Create a new login document using the existing schema
-        const newLogin = new loginModel.Logins({
-            uid: newUID,
-            username: req.body.username,
-            password: hash
-        })
+                //Create a new login document using the existing schema
+                const newLogin = new loginModel.Logins({
+                    uid: newUID,
+                    username: req.body.username,
+                    password: hash
+                })
 
-        //save the new documents, send a response
-        newUser.save().then((response1) => {
-            newLogin.save().then((response2) => {
-                res.status(201).json({
-                    error: error
-                });
-            }).catch(error => {
-                res.status(500).json({
-                    error:error
+                //save the new documents, send a response
+                newUser.save().then((response1) => {
+                    newLogin.save().then((response2) => {
+                        res.status(201).json({
+                            error: error
+                        });
+                    }).catch(error => {
+                        res.status(500).json({
+                            error:error
+                        });
+                    });
+                }).catch(error => {
+                    res.status(500).json({
+                        error:error
+                    });
                 });
             });
-        }).catch(error => {
-            res.status(500).json({
-                error:error
-            });
-        });
-    });
+        }
 });
 
 //export for use in index.js
