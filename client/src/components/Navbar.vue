fb11f1ca559cb6f8e8a7500f55f13e0bdd8c61e6
Connor H <connor.henderson@uoit.net>
2021-04-19 16:43:38 -0400

    added try/catch for navbar to handle navbar error

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -64,7 +64,12 @@ import SearchBar from '@/components/SearchBar.vue'
     if(data == "" || data == undefined){
         data='{}';
     }
-    data = JSON.parse(data);
+    
+    try {
+        data = JSON.parse(data);
+    } catch (e) {
+        console.log(data, e)
+    }
 
 export default {
     components: {
