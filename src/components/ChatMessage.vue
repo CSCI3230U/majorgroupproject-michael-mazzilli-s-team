207e2fac064e6fb781f5a289c905021f8e6cafd8
ammarkhan310 <ammarkhan310@gmail.com>
2021-04-18 21:27:48 -0400

    Messages page

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wrapper">
+        <img :src="message.user.picture" width="50px"/>
+        <div class="user-name"> 
+            <div class="user-name">{{ message.user.firstName }} {{ message.user.lastName }}</div>
+        </div>
+        <span class="message-text">{{ message.text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ChatMessage",
+  props: {
+      message: Object,
+  },
+};
+</script>
+
+<style scoped>
+img {
+  border-radius: 50%;
+  margin-bottom: 1rem;
+}
+
+.wrapper {
+    display: grid;
+    grid-template-columns: 10% 90%;
+    grid-template-rows: 20% 80%;
+}
+
+.user-name {
+    font-weight: normal;
+    font-size: 12px;
+    justify-self: start;
+    padding-left: 0.5em;
+}
+
+.message-text {
+    grid-row: 2;
+    grid-column: 2;
+    text-align: start;
+    background-color: azure;
+    border-radius: 25px;
+    padding: 2px;
+    padding-left: 1em;
+    padding-right: 1em;
+    width: fit-content;
+    height: fit-content;
+}
+</style>
---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="chat-window">
+        <div class="chats tile is-child">
+            <ChatMessage :message="chatLog.message1"/>
+            <ChatMessage :message="chatLog.message2"/>
+            <ChatMessage :message="chatLog.message1"/>
+            <ChatMessage :message="chatLog.message2"/>
+            <ChatMessage :message="chatLog.message1"/>
+            <ChatMessage :message="chatLog.message2"/>
+            <ChatMessage :message="chatLog.message1"/>
+            <ChatMessage :message="chatLog.message2"/>
+            <ChatMessage :message="chatLog.message1"/>
+            <ChatMessage :message="chatLog.message2"/>
+        </div>
+        <div class="tile is-parent"> 
+            <input class="input is-rounded" type="text" placeholder="Type a message">
+        </div>
+    </div>
+</template>
+
+<script>
+import ChatMessage from '@/components/ChatMessage.vue';
+
+export default {
+  name: "ChatWindow",
+  components: {
+      ChatMessage,
+  },
+  props: {
+      chatLog: Object,
+  },
+};
+</script>
+
+<style scoped>
+.chats {
+    position: relative;
+    overflow-y: scroll;
+    max-height: 77vh;
+    padding-right: 2em;
+}
+
+.response {
+    position: absolute;
+    width: 100%;
+    bottom: 0px;
+}
+</style>
