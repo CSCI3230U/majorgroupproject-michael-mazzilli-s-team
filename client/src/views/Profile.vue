0d6f8087274ebd676ba04f7abe568c79149a1358
Justin Katerberg <j.katerberg10@gmail.com>
2021-04-20 02:19:34 -0400

    view all comments from user

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -5,19 +5,22 @@
       <ProfileInfo :user="user"/>
       <div class="menu">
         <ul class="menu-list">
-          <li><a v-on:click="selected='posts'"
+          <li><a v-on:click="selected='posts'; getPosts()"
                   v-bind:class="(isSelected('posts'))?'has-background-light':''">
                   Posts
               </a>
           </li>
-          <li><a v-on:click="selected='comments'"
+          <li><a v-on:click="selected='comments'; getPosts()"
                   v-bind:class="(isSelected('comments'))?'has-background-light':''">
                   Comments
               </a>
           </li>
-          <li><a v-on:click="selected='settings'"
-                  v-bind:class="(isSelected('settings'))?'has-background-light':''">
-          Settings</a></li>
+          <li >
+            <a v-if="this.isAuthorized()" v-on:click="selected='settings'"
+                v-bind:class="(isSelected('settings'))?'has-background-light':''">
+                Settings
+            </a>
+          </li>
         </ul>
       </div>
     </div>
@@ -28,6 +31,11 @@
         <Post :post="post"/>
       </div>
     </div>
+    <div id="comments" v-if="isSelected('comments')" class="tile is-child">
+       <div class="post" v-for="post in posts" :key="post.post_id" :post="post">
+        <Post :post="post"/>
+      </div>
+   </div>
     <div id="info" v-if="isSelected('settings')" class="tile is-child">
       <LoginInfo :user="user" :key="user._id"/>
     </div>
@@ -39,6 +47,8 @@
 import Post from '@/components/Post.vue'
 import ProfileInfo from '@/components/ProfileInfo.vue'
 import LoginInfo from '@/components/LoginInfo.vue'
+var cookie = require('../scripts/cookies');
+var auth = require('../scripts/auth');
 
 export default {
   name: "Profile",
@@ -66,23 +76,43 @@ export default {
             user_data = response;
           }).then(() => {
             this.user = user_data;
-            console.log(this.user);
+            this.getPosts();
 
-            //fetch user's posts
-            fetch(this.$server+'/getpost/user/'+user_data._id)
-              .then(response => response.json())
-              .then(response => {
-                console.log(response);
-                if(Object.entries(response).length != 0){
-                  this.posts = response;
-                }
-              });
           });
+      },
+
+      getPosts(){
+        var dest;
+        if(this.selected == 'posts'){
+          dest = 'post';
+        }else{
+          dest = 'comments'
+        }
+        console.log(dest);
+
+        fetch(this.$server+'/get'+dest+'/user/'+this.user._id)
+          .then(response => response.json())
+          .then(response => {
+            console.log(response);
+            if(Object.entries(response).length != 0){
+              this.posts = response;
+            }
+        });
+      },
+
+      isAuthorized(){
+        var view_id = this.user._id;
+        if(auth.isAuthenticated() && JSON.parse(cookie.getCookie('user')).msg._id === view_id){
+          console.log(true);
+          return true;
+        }else{
+          console.log("statement is false");
+          return false;
+        }
       },
 
       isSelected(item){
         if(this.selected === item){
-          console.log("true");
           return true;
         }
         return false;
--- server/routes/getcomment.js
+++ server/routes/getcomment.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var Comments = require('../models/comment_model');
+var Posts = require('../models/post_model');
 var Users = require('../models/user_model');
 
+/**
+ * Retrieves the comments for a specific post
+ */
 router.get('/getcomments/:id', (req, res) => {
     Comments.find({parent: req.params.id})
         .populate('author')
@@ -15,5 +19,26 @@ router.get('/getcomments/:id', (req, res) => {
             }
         })
 })
+
+/**
+ * Retrieves all the posts with comments from a specific user
+ */
+router.get('/getcomments/user/:id', (req, res) => {
+    console.log("getting comments from user");
+    Comments.find({author: req.params.id})
+        .populate({
+            path: 'parent',
+            populate: [{path: 'author'},{
+                path: 'replies',
+                populate: { path: 'author' }
+            }]
+        }).then((result,err) => {
+            var posts = result.map(function(post){ return post.parent; });
+            console.log(posts);
+            res.send(posts);
+        })
+});
+
+
 
 module.exports = router;
